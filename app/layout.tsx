8dc9727b69b1581915a6d671f49eb900b2945671
gymynnym <gymynnym@gmail.com>
2025-09-07 17:20:52 +0900

    feat(theme): Support theme with next-themes
    - Add `next-themes` dependency
    - Introduce `ThemeProvider`
    - Set html lang to `ko` and enable `suppressHydrationWarning`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/globals.css';
+import { ThemeProvider } from '@/providers/theme';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
@@ -20,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ko" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
