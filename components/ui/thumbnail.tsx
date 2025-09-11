c5e0677f6ea55c02021109ca1299759189a0a1a7
gymynnym <gymynnym@gmail.com>
2025-09-11 15:37:39 +0900

    refactor(ui): Separate thumbnail components
    - Split `Thumbnail` into `Thumbnail` and `InteractiveThumbnail`

--- components/ui/thumbnail.tsx
+++ components/ui/thumbnail.tsx
@@ -10,22 +10,50 @@ export interface ThumbnailProps {
   aspect: ThumbnailAspectRatio;
   src: string;
   alt: string;
-  width: number;
-  height: number;
+  width?: number;
+  height?: number;
+  fill?: boolean;
   loading?: 'eager' | 'lazy';
 }
 
-export default function Thumbnail({ aspect, src, alt, width, height, loading = 'lazy' }: Readonly<ThumbnailProps>) {
+const defaultProps: Pick<ThumbnailProps, 'fill' | 'loading'> = {
+  fill: false,
+  loading: 'lazy',
+};
+
+function Thumbnail(props: ThumbnailProps) {
+  props = {
+    ...defaultProps,
+    ...props,
+    src: buildAssetsUrl(props.src),
+  };
+
   return (
-    <div className={cn('relative bg-muted border rounded-lg overflow-hidden group', ASPECT_OPTIONS[aspect])}>
+    <div className={cn('relative bg-muted border rounded-lg overflow-hidden', ASPECT_OPTIONS[props.aspect])}>
+      <Image {...props} className="object-contain" />
+    </div>
+  );
+}
+
+function InteractiveThumbnail(props: ThumbnailProps) {
+  props = {
+    ...defaultProps,
+    ...props,
+    src: buildAssetsUrl(props.src),
+  };
+
+  return (
+    <div className={cn('relative bg-muted border rounded-lg overflow-hidden group', ASPECT_OPTIONS[props.aspect])}>
+      <div className="absolute inset-0 transition-colors group-hover:bg-black/10" />
       <Image
-        src={buildAssetsUrl(src)}
-        alt={alt}
-        width={width}
-        height={height}
-        loading={loading}
-        className="object-cover rounded-xl transition-[scale] group-hover:scale-115"
+        {...props}
+        className={cn(
+          'transition-[scale] group-hover:scale-105',
+          'absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'
+        )}
       />
     </div>
   );
 }
+
+export { Thumbnail, InteractiveThumbnail };
