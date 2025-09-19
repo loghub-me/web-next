67dc6f7933d49039325dbdffa18ce5b11ff2cae4
gymynnym <gymynnym@gmail.com>
2025-09-19 14:13:13 +0900

    chore(sitemap): Add next-sitemap and redirect search page
    - Add `next-sitemap` to generate sitemap and robots.txt
    - Add logo and symbol images in PNG and WebP formats

---
+++ next-sitemap.config.js
@@ -0,0 +1,29 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: process.env.SITE_URL,
+  priority: 0.7,
+  changefreq: 'daily',
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/', disallow: '/topics' }],
+  },
+  transform: async (config, url) => {
+    const highPriorityUrls = ['/'];
+    const highPriorityPatterns = [/^\/search(\/.*)?$/, /^\/post(\/.*)?$/];
+    const isHighPriority = highPriorityUrls.includes(url) || highPriorityPatterns.some((pattern) => pattern.test(url));
+
+    return {
+      loc: url,
+      changefreq: 'daily',
+      priority: isHighPriority ? 1 : 0.7,
+      lastmod: new Date().toISOString(),
+    };
+  },
+  additionalPaths: async (_config) => {
+    return [
+      { loc: '/search/articles', changefreq: 'daily', priority: 1, lastmod: new Date().toISOString() },
+      { loc: '/search/series', changefreq: 'daily', priority: 1, lastmod: new Date().toISOString() },
+      { loc: '/search/questions', changefreq: 'daily', priority: 1, lastmod: new Date().toISOString() },
+    ];
+  },
+};
